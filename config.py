4a76ce2848072c36d24ccc98f2c8a10d06ce88ba
marsel7official <141441864+marsel7official@users.noreply.github.com>
2023-08-25 18:22:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "24248654"))
+API_HASH = environ.get("API_HASH", "9d4dfa5bd74abc85b26ed4e246ced9f3")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6544362647:AAEsS2h_oaZvBUg_HCsaMATECUPTJhL72kY")
+ADMIN = int(environ.get("ADMIN", "2109732446"))          
+CAPTION = environ.get("CAPTION", "@MarselMovie")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/ea7cb43ee90f8eb83a4fa.jpg")
